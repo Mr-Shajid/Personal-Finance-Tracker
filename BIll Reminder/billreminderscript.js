2dec73c6070cb54989c31aca0c77c26f1fc1afd8
Mr-Shajid <mdshahriarchyshajid.cse@gmail.com>
2025-08-22 12:30:32 +0600

    bill reminder design

---
+++ BIll Reminder/billreminderscript.js
@@ -0,0 +1,17 @@
+// Button click actions
+const payButtons = document.querySelectorAll('.pay-btn, .auto-btn, .pending-btn');
+payButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    alert("Action: " + btn.innerText);
+  });
+});
+
+// Hamburger menu toggle
+const hamburger = document.getElementById('hamburger');
+const sidebar = document.getElementById('sidebar');
+
+hamburger.addEventListener('click', () => {
+  sidebar.classList.toggle('hidden');
+});
+
+
