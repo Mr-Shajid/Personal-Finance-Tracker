a569003eaa638257d858572ee070b2f0e652c847
Mr-Shajid <mdshahriarchyshajid.cse@gmail.com>
2025-09-10 04:07:44 +0600

    c

--- view/BIll Reminder/billreminder.php
+++ view/BIll Reminder/billreminder.php
@@ -20,7 +20,7 @@
   <!-- Sidebar -->
   <div class="sidebar" id="sidebar">
     <h2>Personal Finance Tracker</h2>
-    <div class="nav-item"><a href="../Bill Reminder/billreminder.html">Bill Reminders</a></div>
+    <div class="nav-item"><a href="../Bill Reminder/billreminder.php">Bill Reminders</a></div>
     <div class="nav-item"><a href="../Savings Goals/savings.html">Goals</a></div>
     <div class="nav-item"><a href="../Export Data/exportdata.html">Export Data</a></div>
     <div class="nav-item"><a href="../Role base Access/roleaccess.html">Role Based Access</a></div>
